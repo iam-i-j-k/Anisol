75039409b4e839c1811198dab5a129b87f513935
iam-i-j-k <irfanjankhan7860@gmail.com>
2025-01-25 03:57:11 +0530

    port backend

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const GenerateContentSchema = z.object({
 });
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 'https://anisol.onrender.com';
 
 app.use(cors());
 app.use(express.json());
